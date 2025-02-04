95c57888116bd1021293ab81dddef35abd868953
405786MoroBenjamin <405786@tecnicatura.frc.utn.edu.ar>
2025-02-04 18:02:21 -0300

    Update DeviceServiceTest.java

--- src/test/java/ar/edu/utn/frc/tup/lciii/services/DeviceServiceTest.java
+++ src/test/java/ar/edu/utn/frc/tup/lciii/services/DeviceServiceTest.java
@@ -1,12 +1,32 @@
 package ar.edu.utn.frc.tup.lciii.services;
 
+import ar.edu.utn.frc.tup.lciii.repositories.DeviceRepository;
+import jakarta.persistence.Id;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.web.client.RestTemplate;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 class DeviceServiceTest {
 
+    @Mock
+    private DeviceRepository deviceRepository;
+
+    @Mock
+    private RestTemplate restTemplate;
+
+    @InjectMocks
+    private DeviceService deviceService;
+
+    @BeforeEach
+    void setUp(){
+        MockitoAnnotations.openMocks(this);
+    }
 
     @Test
     void saveDevice() {
@@ -42,6 +62,7 @@ class DeviceServiceTest {
 
     @Test
     void validateDeviceHostname() {
+        when(deviceService.validateDeviceHostname("hostname")).thenReturn(true);
     }
 
     @Test
